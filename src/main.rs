93f763e6e6e3c95a9503cb7da49f2536640ec9ad
Rohan Bafna <rbafna6507@gmail.com>
2026-04-03 22:37:05 -0700

    fixed the speed issue

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@ use macroquad::prelude::*;
 use macroquad::input::get_keys_down;
 
 
-const MOVEMENT_SPEED: f32 = 2000.0;
+const MOVEMENT_SPEED: f32 = 100.0;
 const PLAYER_SPEED: f32 = 220.0;
 const THRUST: f32 = 2.0;
 const TURN_SPEED: f32 = 2.0;
 const MASS: f32 = 100.0;
 const GRAVITY: f32 = 0.008;
+const GROUND_HEIGHT: f32 = 100.0;
 
 
 struct Rocket {
+    w: f32,
+    h: f32,
+
     x: f32,
     y: f32,
     vx: f32,
@@ -25,36 +29,68 @@ struct Rocket {
 }
 
 // TODO: figure out pivot on a point for thrusters
-// TODO: gravity
 // TODO: need a brief menu / keyboard control to reset the game
 // TODO: would be cool to measure how good of a landing we had (eg. angle, speed)
 // TODO: spawn in the rocket with a random downward speed and random orientation (between -90 and 90 degrees)
 // TODO: autopilot
 
-#[macroquad::main("rocket")]
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "My Game".to_owned(),
+        window_width: 1480,   // Preferred width
+        window_height: 1020,   // Preferred height
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
 
 
     
-    let mut rocket: Rocket = Rocket { x: (x), y: (y), vx: (0.0), vy: (0.0), ax: (0.0), ay: (0.0), theta: (0.0), vtheta: (0.0), atheta: (0.0)};
+    let mut rocket: Rocket = Rocket { 
+        x: (x), 
+        y: (y), 
+        vx: (0.0), 
+        vy: (0.0), 
+        ax: (0.0), 
+        ay: (0.0), 
+        theta: (0.0), 
+        vtheta: (0.0), 
+        atheta: (0.0), 
+        w: (20.0), 
+        h: (40.0)
+    };
 
     // spawn items and item state
         // background (stars)
         // rocket (square, with two trapezoids 3 units from middle, two lines diagonal from bottom as landing gear)
         // floor
+    clear_background(WHITE);
+    let ground_y = screen_height() - GROUND_HEIGHT;
 
 
     loop {
 
-        clear_background(WHITE);
+        // clear_background(WHITE);
+
+        draw_rectangle(0.0, ground_y, screen_width(), GROUND_HEIGHT, GRAY);
 
         new_process_input(&mut rocket);
 
         add_gravity(&mut rocket);
 
         update_state(&mut rocket);
+
+
+        // if touching the ground and too fast, exit and say you crashed
+        if (is_crashed(&mut rocket) ) {
+            return ;
+        }
+        // if touching the ground and at acceptable speed, let linger for two seconds, take the velocity at that time and score it
 
         draw_rocket(&mut rocket);
 
@@ -89,7 +125,7 @@ fn add_gravity(rocket: &mut Rocket) {
 }
 
 fn update_state(rocket: &mut Rocket) {
-    let dt = 1.0;
+    let dt = get_frame_time() * MOVEMENT_SPEED;
 
     rocket.vx += rocket.ax * dt;
     rocket.vy += rocket.ay * dt;
@@ -99,15 +135,25 @@ fn update_state(rocket: &mut Rocket) {
     rocket.y += rocket.vy * dt;
     rocket.theta += rocket.vtheta * dt;
 
+    rocket.x = rocket.x % screen_width();
+    rocket.y = rocket.y % screen_height();
+
     rocket.ax = 0.0;
     rocket.ay = 0.0;
     rocket.atheta = 0.0;
 
 }
 
+fn is_crashed(rocket: &mut Rocket) -> bool {
+    let r: Rect = Rect { x: rocket.x, y: rocket.y - 20.0, w: (rocket.w), h: (rocket.h) };
+    let g: Rect = Rect { x: 0.0, y:screen_height() - GROUND_HEIGHT, w:screen_width(), h: GROUND_HEIGHT };
+
+    r.overlaps(&g)
+}
+
 
 
 fn draw_rocket(rocket: &mut Rocket) {
     let params: DrawRectangleParams = DrawRectangleParams { offset: ((0.5, 0.5)).into(), rotation: rocket.theta, color: (BLUE) };
-    draw_rectangle_ex(rocket.x, rocket.y, 20.0, 40.0, params);
+    draw_rectangle_ex(rocket.x, rocket.y, rocket.w, rocket.h, params);
 }
