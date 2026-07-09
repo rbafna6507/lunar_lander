5f59e5b4ec7147ee1dfd6191c87561c34dbbba79
Rohan Bafna <rbafna6507@gmail.com>
2026-07-08 20:00:08 -0700

    much more clean main game loop, need to work on particles and game conditions

--- src/bin/new_main.rs
+++ src/bin/new_main.rs
@@ -4,16 +4,18 @@ mod game;
 use std::vec;
 use macroquad::{prelude::*};
 
-use crate::utils::Vector2D;
+use crate::utils::{Rocket, Vector2D};
 
 
 const MOVEMENT_SPEED: f32 = 100.0;
 const PLAYER_SPEED: f32 = 220.0;
-const THRUST: f32 = 2.0;
-const TURN_SPEED: f32 = 3.0;
+const THRUST: f32 = 2.3;
+const TURN_SPEED: f32 = 0.0003;
 const MASS: f32 = 100.0;
 const GRAVITY: f32 = 0.008;
 const GROUND_HEIGHT: f32 = 100.0;
+const ROCKET_WIDTH: f32 = 20.0;
+const ROCKET_HEIGHT: f32 = 40.0;
 
 
 fn window_conf() -> Conf {
@@ -25,12 +27,11 @@ fn window_conf() -> Conf {
     }
 }
 
-
 #[macroquad::main(window_conf)]
 async fn main() {
+
     let x = screen_width() / 2.0;
     let y = screen_height() / 2.0;
-
 
     let mut rocket: utils::Rocket = utils::Rocket::new(
         utils::Vector2D::new(x, y),
@@ -44,38 +45,82 @@ async fn main() {
         // rocket (square, with two trapezoids 3 units from middle, two lines diagonal from bottom as landing gear)
         // floor
     clear_background(WHITE);
-    let ground_y = screen_height() - GROUND_HEIGHT;
-    let mut particles: Vec<Particle> = vec![];
 
+    // simulation loop
     loop {
+        process_input(&mut rocket);
 
-        // clear_background(WHITE);
-
-        draw_rectangle(0.0, ground_y, screen_width(), GROUND_HEIGHT, GRAY);
-
-        process_input(&mut rocket, &mut particles);
-
-        add_gravity(&mut rocket);
-
-        update_state(&mut rocket);
-
-        draw_text(&format!("Speed: {}", get_magnitude(rocket.vx, rocket.vy)), 20.0, 50.0, 30.0, WHITE);
-
-        draw_particles(&mut particles);
-
-        draw_trajectory(&rocket);
+        update_rocket_state(&mut rocket);
 
         draw_rocket(&mut rocket);
 
-
-        // if touching the ground and too fast, exit and say you crashed
-        if (is_crashed(&mut rocket) && get_magnitude(rocket.vx, rocket.vy) >= 0.9) {
-            break;
-        }
-        // if touching the ground and at acceptable speed, let linger for two seconds, take the velocity at after that time and score it
-
-         next_frame().await;
-
+        next_frame().await;
     }
 
 }
+
+fn process_input(rocket: &mut Rocket) {
+    let delta_time = 1.0;
+
+    if is_key_down(KeyCode::W) {
+        rocket.acceleration.y -= delta_time * (THRUST * rocket.rotation.theta.cos()) / MASS;
+        rocket.acceleration.x += delta_time * (THRUST * rocket.rotation.theta.sin()) / MASS;
+    }
+
+    if is_key_down(KeyCode::A) {
+        rocket.rotation.atheta -= delta_time * TURN_SPEED;
+    }
+
+    if is_key_down(KeyCode::D) {
+        rocket.rotation.atheta += delta_time * TURN_SPEED;
+    }
+
+    if is_key_down(KeyCode::S) {
+        rocket.acceleration.y += delta_time * (THRUST * rocket.rotation.theta.cos()) / MASS;
+        rocket.acceleration.x -= delta_time * (THRUST * rocket.rotation.theta.sin()) / MASS;
+    }
+
+    rocket.acceleration.y += GRAVITY;
+
+}
+
+fn update_rocket_state(rocket: &mut Rocket) {
+    let dt = get_frame_time() * MOVEMENT_SPEED;
+
+    // update rocket velocity state
+    rocket.velocity.x += rocket.acceleration.x * dt;
+    rocket.velocity.y += rocket.acceleration.y * dt;
+    
+    // update rocket position
+    rocket.position.x += rocket.velocity.x * dt;
+    rocket.position.y += rocket.velocity.y * dt;
+
+    // update rocket rotational velocity and angle
+    rocket.rotation.vtheta += rocket.rotation.atheta * dt;
+    rocket.rotation.theta += rocket.rotation.vtheta * dt;
+
+    // Allow rocket x axis wrap-around
+    rocket.position.x = rocket.position.x % screen_width();
+
+    // reset acceleration values
+    rocket.acceleration.x = 0.0;
+    rocket.acceleration.y = 0.0;
+    rocket.rotation.atheta = 0.0;
+}
+
+fn get_magnitude(x: f32, y:f32) -> f32 {
+    (x.powf(2.0) + y.powf(2.0)).sqrt()
+}
+
+fn draw_rocket(rocket: &mut utils::Rocket) {
+
+    // Add Ground
+    let ground_y: f32 = screen_height() - GROUND_HEIGHT;
+    draw_rectangle(0.0, ground_y, screen_width(), GROUND_HEIGHT, GRAY);
+
+    draw_text(&format!("Speed: {}", get_magnitude(rocket.velocity.x, rocket.velocity.y) * 9.5), 20.0, 50.0, 30.0, WHITE);
+
+    let params: DrawRectangleParams = DrawRectangleParams { offset: ((0.5, 0.5)).into(), rotation: rocket.rotation.theta, color: (Color::new(125 as f32, 124 as f32, 124 as f32, 1.0)) };
+    draw_rectangle_ex(rocket.position.x, rocket.position.y, ROCKET_WIDTH, ROCKET_HEIGHT, params);
+}
+
--- src/bin/utils.rs
+++ src/bin/utils.rs
@@ -2,8 +2,8 @@
 
 #[derive(Debug, Clone, Copy)]
 pub struct Vector2D {
-    x: f32,
-    y: f32,
+    pub x: f32,
+    pub y: f32,
 }
 
 impl Vector2D {
@@ -11,20 +11,13 @@ impl Vector2D {
     pub fn new(x: f32, y: f32) -> Self { 
         Self { x, y } 
     }
-
-    pub fn x(&self) -> f32 { 
-        self.x 
-    }
-    pub fn y(&self) -> f32 { 
-        self.y 
-    }
 }
 
 #[derive(Debug, Clone, Copy)]
 pub struct RotationalState {
-    theta: f32,
-    vtheta: f32,
-    atheta: f32
+    pub theta: f32,
+    pub vtheta: f32,
+    pub atheta: f32
 }
 
 impl RotationalState {
@@ -33,46 +26,18 @@ impl RotationalState {
         Self {theta, vtheta, atheta}
     }
 
-    pub fn theta(&self) -> f32 {
-        self.theta
-    }
-
-    pub fn vtheta(&self) -> f32 {
-        self.vtheta
-    }
-
-    pub fn atheta(&self) -> f32 {
-        self.atheta
-    }
-
 }
 
 pub struct Rocket {
-    position: Vector2D,
-    velocity: Vector2D,
-    acceleration: Vector2D,
-    theta: RotationalState
+    pub position: Vector2D,
+    pub velocity: Vector2D,
+    pub acceleration: Vector2D,
+    pub rotation: RotationalState
 }
 
 impl Rocket {
-    pub fn new(position: Vector2D, velocity: Vector2D, acceleration: Vector2D, theta: RotationalState) -> Rocket {
-        Self {position, velocity, acceleration, theta}
-    }
-
-    pub fn position(&self) -> Vector2D {
-        self.position
-    }
-
-    pub fn velocity(&self) -> Vector2D {
-        self.velocity
-    }
-
-    pub fn acceleration(&self) -> Vector2D {
-        self.acceleration
-    }
-
-    pub fn theta(&self) -> RotationalState {
-        self.theta
+    pub fn new(position: Vector2D, velocity: Vector2D, acceleration: Vector2D, rotation: RotationalState) -> Rocket {
+        Self {position, velocity, acceleration, rotation}
     }
 }
 
